4fdf04fda714a82cefb0796b82924597ad487e72
Farah Nisar <farah.nisar06@gmail.com>
2018-12-17 02:09:54 +0100

    Adding more fields to Movie class, Added endpoints to update a movie and to find movies by genre

--- src/main/java/movie/Movie.java
+++ src/main/java/movie/Movie.java
@@ -3,6 +3,13 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.joda.time.DateTime;
 
+/**
+ * Movie entity with an array of possible fields. The id, title, releaseDate,
+ * language and runningTime fields are required for the movie model, the rest
+ * are optional and added for completeness.
+ * 
+ * @author farahnisar
+ */
 public class Movie {
 	@NotEmpty
 	private long id;
@@ -27,17 +34,61 @@ public class Movie {
 	
 	private String[] genres;
 	
+	private float userRating;
+
+	private float criticsRating;
+
+	/*
+	 * URL to the movie's poster image. Can be stored in some FTP of cloud file
+	 * system.
+	 */
+	private String posterImageUrl;
+
+	private String trailerUrl;
+
+	private String website;
+
+	private String studioName;
+
+	private Actor[] cast;
+
+	private Director[] directors;
+
+	private String[] awardNominations;
+
 	public Movie() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public Movie(long id, String title, DateTime releaseDate, String language, int runningTime) {
-		super();
 		this.id = id;
 		this.title = title;
 		this.releaseDate = releaseDate;
 		this.language = language;
 		this.runningTime = runningTime;
+	}
+
+	public Movie(long id, String title, DateTime releaseDate, String language, String countryOfOrigin, int runningTime,
+			String description, String[] genres, float userRating, float criticsRating, String posterImageUrl,
+			String trailerUrl, String website, String studioName, Actor[] cast, Director[] directors,
+			String[] awardNominations) {
+		this.id = id;
+		this.title = title;
+		this.releaseDate = releaseDate;
+		this.language = language;
+		this.countryOfOrigin = countryOfOrigin;
+		this.runningTime = runningTime;
+		this.description = description;
+		this.genres = genres;
+		this.userRating = userRating;
+		this.criticsRating = criticsRating;
+		this.posterImageUrl = posterImageUrl;
+		this.trailerUrl = trailerUrl;
+		this.website = website;
+		this.studioName = studioName;
+		this.cast = cast;
+		this.directors = directors;
+		this.awardNominations = awardNominations;
 	}
 
 	public long getId() {
@@ -103,6 +154,77 @@ public class Movie {
 	public void setGenres(String[] genres) {
 		this.genres = genres;
 	}
-	
-	
+
+	public float getUserRating() {
+		return userRating;
+	}
+
+	public void setUserRating(float userRating) {
+		this.userRating = userRating;
+	}
+
+	public float getCriticsRating() {
+		return criticsRating;
+	}
+
+	public void setCriticsRating(float criticsRating) {
+		this.criticsRating = criticsRating;
+	}
+
+	public String getPosterImageUrl() {
+		return posterImageUrl;
+	}
+
+	public void setPosterImageUrl(String posterImageUrl) {
+		this.posterImageUrl = posterImageUrl;
+	}
+
+	public String getTrailerUrl() {
+		return trailerUrl;
+	}
+
+	public void setTrailerUrl(String trailerUrl) {
+		this.trailerUrl = trailerUrl;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public String getStudioName() {
+		return studioName;
+	}
+
+	public void setStudioName(String studioName) {
+		this.studioName = studioName;
+	}
+
+	public Actor[] getCast() {
+		return cast;
+	}
+
+	public void setCast(Actor[] cast) {
+		this.cast = cast;
+	}
+
+	public Director[] getDirectors() {
+		return directors;
+	}
+
+	public void setDirectors(Director[] directors) {
+		this.directors = directors;
+	}
+
+	public String[] getAwardNominations() {
+		return awardNominations;
+	}
+
+	public void setAwardNominations(String[] awardNominations) {
+		this.awardNominations = awardNominations;
+	}
+
 }
--- src/main/java/resources/MovieResource.java
+++ src/main/java/resources/MovieResource.java
@@ -1,14 +1,18 @@
 package resources;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 import movie.Movie;
 
@@ -39,5 +43,32 @@ public class MovieResource {
 	@Path("{id}")
 	public void deleteMovie(@PathParam("id") long movieId) {
 		MOVIES_COLLECTION.remove(movieId);
+	}
+
+	@PUT
+	@Path("{id}")
+	public void updateMovie(@PathParam("id") long movieId, Movie newMovie) {
+		MOVIES_COLLECTION.remove(movieId);
+		MOVIES_COLLECTION.put(movieId, newMovie);
+	}
+
+	@GET
+	@Path("/genre/{genre}")
+	public ArrayList<Movie> findMovieByGenre(@PathParam("genre") String genre) {
+		/*
+		 * Note: Since here we have stored the movies in memory in a map instead of some
+		 * persistent database, our findBy* method loops through the collection and
+		 * looks for the movie with the desired property.
+		 * 
+		 * Normally with some database at the backedn, Hibernate Criterias can be easily
+		 * used to search for data which comes baked-in with the DropWizard framework.
+		 * 
+		 */
+		ArrayList<Movie> movies = new ArrayList<Movie>();
+		for (Movie m : MOVIES_COLLECTION.values()) {
+			if (ArrayUtils.contains(m.getGenres(), genre))
+				movies.add(m);
+		}
+		return movies;
 	}
 }
